1e4205a5c58ef8088998940427aede02a2287e56
Federico Bond <federicobond@gmail.com>
2015-04-11 16:49:18 -0300

    Initial commit

---
+++ src/main/java/ar/edu/itba/it/gossip/App.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.it.gossip;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ar/edu/itba/it/gossip/AppTest.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.it.gossip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void trueStatement() {
+        assertTrue( true );
+    }
+}
