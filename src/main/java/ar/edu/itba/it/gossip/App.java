6653c03a9a546312b3444ad762e69062d7dbbd91
Federico Bond <federicobond@gmail.com>
2015-04-12 21:43:04 -0300

    Add logging libraries

--- src/main/java/ar/edu/itba/it/gossip/App.java
+++ src/main/java/ar/edu/itba/it/gossip/App.java
@@ -1,7 +1,11 @@
 package ar.edu.itba.it.gossip;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main( String[] args ) {
+        Logger log = LoggerFactory.getLogger(App.class);
+        log.info("Hello world!");
+    }
 }
--- src/test/java/ar/edu/itba/it/gossip/AppTest.java
+++ src/test/java/ar/edu/itba/it/gossip/AppTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class AppTest
-{
+public class AppTest {
     /**
      * Rigourous Test :-)
      */
