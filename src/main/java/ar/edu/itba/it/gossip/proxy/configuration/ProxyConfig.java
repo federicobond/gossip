43cc01aa39b0318597bbb4cdca56d5597e0e8093
kpjjpk <juanpabloreyv@gmail.com>
2015-06-09 17:55:30 -0300

    \t -> '    ', as per Java convention

--- src/main/java/ar/edu/itba/it/gossip/proxy/configuration/ProxyConfig.java
+++ src/main/java/ar/edu/itba/it/gossip/proxy/configuration/ProxyConfig.java
@@ -9,118 +9,124 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ProxyConfig {
-	private static final ProxyConfig INSTANCE = new ProxyConfig();
-	private static final String CONFIG_PATH = "/proxy.properties";
+    private static final ProxyConfig INSTANCE = new ProxyConfig();
+    private static final String CONFIG_PATH = "/proxy.properties";
 
-	private static final Logger log = Logger.getLogger("ProxyConfig");
+    private static final Logger log = Logger.getLogger("ProxyConfig");
 
-	private String adminUser;
-	private String adminPassword;
+    private String adminUser;
+    private String adminPassword;
 
-	private String defaultOriginHost;
-	private int defaultOriginPort;
-	
-	private Map<String,String> userToOrigin = new HashMap<String,String>();
-	private Set<String> silencedUsers = new HashSet<String>();
-	private boolean convertLeet = false;
-	
-	private AtomicLong bytesWritten = new AtomicLong();
-	private AtomicLong bytesRead = new AtomicLong();
-	private AtomicLong accessCount = new AtomicLong();
-	private AtomicLong messagesSent = new AtomicLong();
-	private AtomicLong messagesReceived = new AtomicLong();
-	private AtomicLong messagesMutedOutgoing = new AtomicLong();
-	private AtomicLong messagesMutedIngoing = new AtomicLong();
+    private String defaultOriginHost;
+    private int defaultOriginPort;
 
- 	private ProxyConfig() {
-		Properties properties = new Properties();
+    private Map<String, String> userToOrigin = new HashMap<String, String>();
+    private Set<String> silencedUsers = new HashSet<String>();
+    private boolean convertLeet = false;
 
-		InputStream is = getClass().getResourceAsStream(CONFIG_PATH);
-		try {
-			properties.load(is);
-		} catch (IOException e) {
-			log.log(Level.SEVERE, "Could not load proxy properties.");
-			System.exit(1);
-		}
-		load(properties);
-	}
+    private AtomicLong bytesWritten = new AtomicLong();
+    private AtomicLong bytesRead = new AtomicLong();
+    private AtomicLong accessCount = new AtomicLong();
+    private AtomicLong messagesSent = new AtomicLong();
+    private AtomicLong messagesReceived = new AtomicLong();
+    private AtomicLong messagesMutedOutgoing = new AtomicLong();
+    private AtomicLong messagesMutedIngoing = new AtomicLong();
 
-	private void load(Properties properties) {
-		adminUser = properties.getProperty("admin.user", "admin");
-		adminPassword = properties.getProperty("admin.password", "1234");
-		convertLeet = properties.getProperty("leet.default", "false").equals("true");
-		defaultOriginHost = properties.getProperty("origin.default.host", "localhost");
-		defaultOriginPort = Integer.parseInt(properties.getProperty("origin.default.port", "5222"));
+    private ProxyConfig() {
+        Properties properties = new Properties();
 
-		String[] users = properties.getProperty("users.silenced").split(",");
-		for (String user : users) {
-			user = user.trim();
-			if (!user.equals("")) {
-				silencedUsers.add(user);
-			}
-		}
+        InputStream is = getClass().getResourceAsStream(CONFIG_PATH);
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Could not load proxy properties.");
+            System.exit(1);
+        }
+        load(properties);
+    }
 
-		users = properties.getProperty("users.multiplexed").split(",");
-		for (String user : users) {
-			user = user.trim();
-			if (!user.equals("")) {
-				String[] parts = user.split("@");
-				if (parts.length != 2) {
-					log.log(Level.WARNING, "Skipped invalid multiplexed user value: ", user);
-					continue;
-				}
-				userToOrigin.put(parts[0], parts[1]);
-			}
-		}
-	}
-	
-	public static ProxyConfig getInstance() {
-		return INSTANCE;
-	}
-	
-	//Assumes all servers have the same name as address
-	public InetSocketAddress getOriginAddress(String username){
-		// Assumes that all origin servers listen in port 5222 (XMPP port)
-		if(userToOrigin.containsKey(username)){
-				return new InetSocketAddress(userToOrigin.get(username), defaultOriginPort);
-		}
-		return new InetSocketAddress(defaultOriginHost, defaultOriginPort);
-	}
-	
-	public String getOriginName(String username){
-		if(userToOrigin.containsKey(username)){
-			return userToOrigin.get(username);
-		}
-		return defaultOriginHost;
-	}
-	
-	public void addOrigin(String username, String origin){
-	    userToOrigin.put(username, origin);
-	}
-	public String getOriginName(){
-		return defaultOriginHost;
-	}
-	
-	public boolean convertLeet(){
-	    return this.convertLeet;
-	}
-	
-	public void setLeet(boolean convertLeet){
-	    this.convertLeet = convertLeet;
-	    return;
-	}
-	
-	public void silence(String user){
-	    silencedUsers.add(user.trim().toLowerCase());
-	}
-	
-	public void unsilence(String user){
+    private void load(Properties properties) {
+        adminUser = properties.getProperty("admin.user", "admin");
+        adminPassword = properties.getProperty("admin.password", "1234");
+        convertLeet = properties.getProperty("leet.default", "false").equals(
+                "true");
+        defaultOriginHost = properties.getProperty("origin.default.host",
+                "localhost");
+        defaultOriginPort = Integer.parseInt(properties.getProperty(
+                "origin.default.port", "5222"));
+
+        String[] users = properties.getProperty("users.silenced").split(",");
+        for (String user : users) {
+            user = user.trim();
+            if (!user.equals("")) {
+                silencedUsers.add(user);
+            }
+        }
+
+        users = properties.getProperty("users.multiplexed").split(",");
+        for (String user : users) {
+            user = user.trim();
+            if (!user.equals("")) {
+                String[] parts = user.split("@");
+                if (parts.length != 2) {
+                    log.log(Level.WARNING,
+                            "Skipped invalid multiplexed user value: ", user);
+                    continue;
+                }
+                userToOrigin.put(parts[0], parts[1]);
+            }
+        }
+    }
+
+    public static ProxyConfig getInstance() {
+        return INSTANCE;
+    }
+
+    // Assumes all servers have the same name as address
+    public InetSocketAddress getOriginAddress(String username) {
+        // Assumes that all origin servers listen in port 5222 (XMPP port)
+        if (userToOrigin.containsKey(username)) {
+            return new InetSocketAddress(userToOrigin.get(username),
+                    defaultOriginPort);
+        }
+        return new InetSocketAddress(defaultOriginHost, defaultOriginPort);
+    }
+
+    public String getOriginName(String username) {
+        if (userToOrigin.containsKey(username)) {
+            return userToOrigin.get(username);
+        }
+        return defaultOriginHost;
+    }
+
+    public void addOrigin(String username, String origin) {
+        userToOrigin.put(username, origin);
+    }
+
+    public String getOriginName() {
+        return defaultOriginHost;
+    }
+
+    public boolean convertLeet() {
+        return this.convertLeet;
+    }
+
+    public void setLeet(boolean convertLeet) {
+        this.convertLeet = convertLeet;
+        return;
+    }
+
+    public void silence(String user) {
+        silencedUsers.add(user.trim().toLowerCase());
+    }
+
+    public void unsilence(String user) {
         silencedUsers.remove(user.trim().toLowerCase());
     }
-	
-	public boolean isSilenced(String user){
-	    return silencedUsers.contains(user.trim().toLowerCase());
-	}
+
+    public boolean isSilenced(String user) {
+        return silencedUsers.contains(user.trim().toLowerCase());
+    }
 
     public String getAdminUser() {
         return adminUser;
@@ -130,59 +136,59 @@ public class ProxyConfig {
         return adminPassword;
     }
 
-	public void countWrites(int written){
-	    this.bytesWritten.addAndGet(written);
-	}
+    public void countWrites(int written) {
+        this.bytesWritten.addAndGet(written);
+    }
 
-	public long getWrittenBytes(){
-	    return this.bytesWritten.get();
-	}
+    public long getWrittenBytes() {
+        return this.bytesWritten.get();
+    }
 
-	public void countAccess(){
-	    this.accessCount.incrementAndGet();
-	}
+    public void countAccess() {
+        this.accessCount.incrementAndGet();
+    }
 
-	public long getAccesses(){
-	    return this.accessCount.get();
-	}
+    public long getAccesses() {
+        return this.accessCount.get();
+    }
 
-	public void countReads(int read){
-	    this.bytesRead.addAndGet(read);
-	}
+    public void countReads(int read) {
+        this.bytesRead.addAndGet(read);
+    }
 
-	public long getReadBytes(){
-	    return this.bytesRead.get();
-	}
+    public long getReadBytes() {
+        return this.bytesRead.get();
+    }
 
-	public void countSentMessage(){
-	    this.messagesSent.incrementAndGet();
-	}
+    public void countSentMessage() {
+        this.messagesSent.incrementAndGet();
+    }
 
-	public long getSentMessagesCount(){
-	    return this.messagesSent.get();
-	}
+    public long getSentMessagesCount() {
+        return this.messagesSent.get();
+    }
 
-	public void countReceivedMessage(){
+    public void countReceivedMessage() {
         this.messagesReceived.incrementAndGet();
     }
 
-    public long getReceivedMessagesCount(){
+    public long getReceivedMessagesCount() {
         return this.messagesReceived.get();
     }
 
-    public void countMessageMutedIn(){
+    public void countMessageMutedIn() {
         this.messagesMutedIngoing.incrementAndGet();
     }
 
-    public long getMessagesMutedIn(){
+    public long getMessagesMutedIn() {
         return this.messagesMutedIngoing.get();
     }
 
-    public void countMessageMutedOut(){
+    public void countMessageMutedOut() {
         this.messagesMutedOutgoing.incrementAndGet();
     }
 
-    public long getMessagesMutedOut(){
+    public long getMessagesMutedOut() {
         return this.messagesMutedOutgoing.get();
     }
 
